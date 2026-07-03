5001b10b798622dce79b4716c86f777471c53ae4
Ak1ra-git <mateusoliveiranascimento28@gmail.com>
2026-07-03 14:39:58 -0300

    Convert frontend to normal folder

---
+++ frontend/src/app/Produtos/page.tsx
@@ -0,0 +1,7 @@
+export default function ProdutosPage() {
+  return (
+    <div>
+      <h1>Cadastro de Produtos</h1>
+    </div>
+  );
+}
---
+++ frontend/src/components/layout/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header className="h-16 bg-white border-b border-gray-200 flex items-center justify-between px-6">
+      <h1 className="text-2xl font-bold text-gray-800">
+        PC Inventory
+      </h1>
+
+      <span className="text-gray-600">
+        Mateus
+      </span>
+    </header>
+  );
+}
